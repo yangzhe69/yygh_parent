93fef46fa0f532feefb4cd3517817d5e45ae2ca5
yangzhe <289264969@qq.com>
2023-04-12 14:13:26 +0800

    2023.04.12 company提交

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -2,8 +2,10 @@ package com.atguigu.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="com.atguigu")
 public class ServiceHospApplication {
 
     public static void main(String[] args) {
--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -1,10 +1,16 @@
 package com.atguigu.yygh.hosp.controller;
 
+import com.atguigu.yygh.common.result.Result;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -13,7 +19,27 @@ public class HospitalSetController {
     private HospitalSetService hospitalSetService;
 
 
+    //查询医院设置表里的所有信息
+    @ApiOperation(value = "获取所有医院设置")
+    @GetMapping("findAll")
+    public Result findAllHosptial() {
+        List<HospitalSet> list = hospitalSetService.list();
+
+        return Result.ok(list);
 
 
+    }
+
+    //删除医院设置
+    @ApiOperation(value = "逻辑删除医院设置")
+    @DeleteMapping("{id}")
+    public Result deleteHospSet(@PathVariable Long id) {
+         boolean flag = hospitalSetService.removeById(id);
+        if(flag) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
 
 }
