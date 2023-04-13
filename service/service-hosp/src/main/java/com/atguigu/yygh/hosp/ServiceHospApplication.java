fbe9a99309b4494ec637208819e2abd086af90d6
yangzhe <289264969@qq.com>
2023-04-13 16:11:25 +0800

    2023.04.13 company提交

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -3,11 +3,13 @@ package com.atguigu.yygh.hosp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.atguigu")
 @EnableDiscoveryClient
+@EnableFeignClients("com.atguigu")
 public class ServiceHospApplication {
 
     public static void main(String[] args) {
